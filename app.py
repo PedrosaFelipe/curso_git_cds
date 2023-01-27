75b9d468c883f3f6e0dddd9206c9811a67612dee
FELIPE S PEDROSA <55566708+PedrosaFelipe@users.noreply.github.com>
2023-01-26 23:14:52 -0300

    Resolvido conflito de merge

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ st.set_page_config(layout="wide")
 
 
 def create_dataframe_section(df):
-    
-    st.title("Sctions - Database Description")
+
+    st.title("Actions - Database Description")
 
     col_1, col_2 = st.columns(2)
 
     col_1.header("Database")
+    
     col_1.dataframe(df, height=530)
 
     col_2.header("Data Description")
@@ -43,27 +44,19 @@ def create_answers_section(df):
     st.title("Main Questions Answers")
 
     st.header("First Round")
-    st.subheader(
-        "How many bikes are being sold by their owners and how many bikes are being sold by distributors?"
-    )
+    st.subheader( "How many bikes are being sold by their owners and how many bikes are being sold by distributors?"  )
     asw.rd1_question_9(df)
 
     st.subheader("How many bikes are being sold are bikes from a unique owner?")
     asw.rd1_question_13(df)
 
-    st.subheader(
-        "Are high kilometer bikes more expensive than bikes with lower kilometer?"
-    )
+    st.subheader( "Are high kilometer bikes more expensive than bikes with lower kilometer?"  )
     asw.rd1_question_14(df)
 
-    st.subheader(
-        "Are the bikes with a unique owner more expense on avarege than the other bikes?"
-    )
+    st.subheader( "Are the bikes with a unique owner more expense on avarege than the other bikes? "   )
     asw.rd2_question_1(df)
 
-    st.subheader(
-        "Are the bikes that have more owners also the bikes with more kilometers traveled on avarege?"
-    )
+    st.subheader(  "Are the bikes that have more owners also the bikes with more kilometers traveled on avarege?" )
     asw.rd2_question_2(df)
 
     st.subheader("Which company has the most bikes registered?")
@@ -72,9 +65,7 @@ def create_answers_section(df):
     st.subheader("Which company has the most expensive bikes on avarege?")
     asw.rd3_question_2(df)
 
-    st.subheader(
-        "Are the company that has the most expensive bikes registered also the company with the most bikes registered?"
-    )
+    st.subheader( "Are the company that has the most expensive bikes registered also the company with the most bikes registered?" )
     asw.rd3_question_5(df)
 
     st.subheader("Which bikes are good for buying?")
@@ -82,11 +73,15 @@ def create_answers_section(df):
 
 
 def create_main_layout():
-    df = load_data()
+                 
+    df_raw = load_data()
+    
+    create_dataframe_section(df_raw)
+    
+    create_answers_section(df_raw)
 
-    create_dataframe_section(df)
+    return None
 
-    create_answers_section(df)
 
 
 if __name__ == "__main__":
